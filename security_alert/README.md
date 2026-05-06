6b2d9a603c7479c847d7f076241c8453e53ca796
calebsenm <calebsenm@gmail.com>
2026-05-06 14:21:16 -0500

    first commit

---
+++ security_alert/README.md
@@ -0,0 +1,13 @@
+
+# This project uses.
+
++ Java:25
++ gradle ktl
++ yml 
+
+For configure the project. create the archive `seccrets.properties`
+add the db credencials and run. 
+
+```
+./gradlew bootRun
+```
---
+++ security_alert/src/main/java/backend/security_alert/SecurityAlertApplication.java
@@ -0,0 +1,13 @@
+package backend.security_alert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityAlertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityAlertApplication.class, args);
+	}
+
+}
